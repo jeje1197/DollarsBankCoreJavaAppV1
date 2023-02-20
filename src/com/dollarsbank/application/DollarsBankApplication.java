9cbe95db6d65c1b51716ceee98d3faf971f3a473
Joseph E <1jojorun@gmail.com>
2023-02-20 14:38:08 -0800

    Implementing Single Responsibility Principle for Utilities

--- src/com/dollarsbank/application/DollarsBankApplication.java
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -1,28 +1,45 @@
 package com.dollarsbank.application;
 
+import com.dollarsbank.controller.DollarsBankController;
+import com.dollarsbank.exception.InvalidOptionException;
 import com.dollarsbank.utility.ColorPrinter;
 import com.dollarsbank.utility.OptionSelector;
+import com.dollarsbank.utility.PrettyFormatter;
 
 public class DollarsBankApplication {
 	
 	public static void main(String[] args) throws Exception {
+		boolean applicationRunning = true;
+		
 		printWelcome();
-		printOptions();
-		ColorPrinter.print(ColorPrinter.ANSI_GREEN,  "Enter choice (1, 2, or 3):\n");
-		int selectedOption = -1; 
-		try {
-			selectedOption = OptionSelector.pickOption(1, 3, "Invalid Option: try again.");
-		} catch (Exception e) {
-			ColorPrinter.print(ColorPrinter.ANSI_RED, null);
+		while (applicationRunning) {
+			printOptions();
+
+			int selectedOption = -1; 
+			try {
+				ColorPrinter.print(ColorPrinter.ANSI_GREEN,  "Enter choice (1, 2, or 3):\n");
+				selectedOption = OptionSelector.pickOption(1, 3, "Invalid Option: try again.");
+			} catch (InvalidOptionException e) {
+				ColorPrinter.print(ColorPrinter.ANSI_RED, e.getMessage());
+				continue;
+			}
+			
+			switch(selectedOption) {
+			case 1:
+				DollarsBankController.createNewAccount();
+				break;
+			case 2:
+				DollarsBankController.login();
+				break;
+			case 3:
+				applicationRunning = false;
+				break;
+			}
 		}
 	}
 	
 	private static void printWelcome() throws Exception {
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("+---------------------------+\n");
-		stringBuilder.append("| DOLLARSBANK WELCOMES YOU! |\n");
-		stringBuilder.append("+---------------------------+\n");
-		ColorPrinter.print(ColorPrinter.ANSI_BLUE, stringBuilder.toString());
+		ColorPrinter.print(ColorPrinter.ANSI_BLUE, PrettyFormatter.format("DOLLARSBANK WELCOMES YOU!"));
 	}
 	
 	private static void printOptions() throws Exception {
--- src/com/dollarsbank/controller/DollarsBankController.java
+++ src/com/dollarsbank/controller/DollarsBankController.java
@@ -1,5 +1,16 @@
 package com.dollarsbank.controller;
 
+import com.dollarsbank.model.Account;
+
 public class DollarsBankController {
+	Account userAccount;
 	
+	// Select Options
+	public static void createNewAccount() {
+//		userAccount = new Account();
+	}
+	
+	public static void login() {
+			
+	}
 }
